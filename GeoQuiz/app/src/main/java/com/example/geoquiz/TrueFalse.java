53f2657de3b6859bbb32aa0a1c905b1a4b1acd3a
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-02-25 15:38:17 -0500

    Adding true/false class
    Adding true/false class to standardize question and answer pairs

---
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/TrueFalse.java
@@ -0,0 +1,12 @@
+package com.example.geoquiz;
+
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int mQuestion, boolean mTrueQuestion) {
+        this.mQuestion = mQuestion;
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
