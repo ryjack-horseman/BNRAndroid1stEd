8016783a606d906c1c1930774bef2f413efccbd0
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-03-02 17:49:02 -0500

    Add question logic on next button press
    Added logic to iterate through the questions on next button press

--- GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -5,16 +5,31 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+      new TrueFalse(R.string.question_oceans, true),
+      new TrueFalse(R.string.question_mideast, false),
+      new TrueFalse(R.string.question_africa, false),
+      new TrueFalse(R.string.question_americas, true),
+      new TrueFalse(R.string.question_asia, true)
+    };
+
+    private int mCurrentIdx = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +46,22 @@ public class QuizActivity extends Activity {
                 Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIdx = (mCurrentIdx + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIdx].getQuestion();
+        mQuestionTextView.setText(question);
     }
 
     //not identical to book, book inflates: getMenuInflator.inflate(R.menu.activity_quiz, menu) but
