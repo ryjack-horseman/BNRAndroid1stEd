441b04d495f591358f08c86aa41cf5af4a2def4a
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-03-05 09:41:10 -0500

    Add logic to cheat
    Add logic to respond to button click and display the correct answer

--- GeoQuiz/app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -2,11 +2,17 @@ package com.example.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends Activity {
     public static final String EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz.answer_is_true";
 
     private boolean mAnswerIsTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,19 @@ public class CheatActivity extends Activity {
         setContentView(R.layout.activity_cheat);
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+
+        mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                }else{
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
 }
